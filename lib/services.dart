cba8e4fc78966c7e028faeb46c43881293a88381
Mika Andrianarijaona <mikaoelitiana@gmail.com>
2022-01-26 17:55:00 +0100

    fix: update supabase app

--- lib/services.dart
+++ lib/services.dart
@@ -11,8 +11,8 @@ class Services extends InheritedWidget {
   }) : super(child: child);
 
   factory Services({required Widget child}) {
-    final client = SupabaseClient('https://yhogmhunpfaxhswbeify.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQyNjAzMDM2LCJleHAiOjE5NTgxNzkwMzZ9.kF5jOV1QJRuQHjY8b4CBcfK0sAw3QIJVe1Kw0irOxfw');
+    final client = SupabaseClient('https://ykzdagzovjznuvucvhgb.supabase.co',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNjQzMjEzNTI3LCJleHAiOjE5NTg3ODk1Mjd9.J_in1uH_kJOfyZE6oFYV6kfvphTEUcOG-JiYSbaDxVk');
     final pomodorosService = PomodorosService(client);
     return Services._(pomodorosService: pomodorosService, child: child);
   }
